0a237b9c7f06f7a99901335aa06d6bc867471311
burpmonster <lucas.c.haskell@gmail.com>
2023-04-21 16:39:32 -0400

    removed all recent scripts

--- index.js
+++ index.js
@@ -1,22 +1,6 @@
 import dotenv from 'dotenv';
 import * as filestack from 'filestack-js';
-import * as http from 'http';
 dotenv.config();
-// import express from 'express';
-
-// const app = express()
-const hostname = '127.0.0.1'
-const port = process.env.PORT
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'text/plain')
-  res.end(process.env.GREETING)
-})
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`)
-})
 
 
 const API_KEY = process.env.API_KEY
@@ -27,12 +11,7 @@ const options = {
 
 client.picker(options).open();
 
-if (typeof window !== 'undefined') {
-    console.log('You are on the browser,You are good to go')
-
-// window.addEventListener('DOMContentLoaded',
-
-const fileStackFunc = function() {
+window.addEventListener('DOMContentLoaded', function() {
 
 
     const apikey = API_KEY;
@@ -65,8 +44,4 @@ const fileStackFunc = function() {
           console.log(err)
         });
     });
-  };
-
-} else {
-    console.log('You are on the server,Cannot execute')
-   }
+  });
